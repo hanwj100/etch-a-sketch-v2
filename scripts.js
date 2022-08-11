d93616df6d7cd3b4a50f4b1c7fe647c7d396c5ed
Woo Han <hanwj100@gmail.com>
2022-08-11 16:01:18 -0700

    Make movePen function operational
    Also create colorInCell function.

--- scripts.js
+++ scripts.js
@@ -1,39 +1,50 @@
 createGrid();
 const grid = document.querySelector("#grid");
-
+let pen = document.querySelector("[data-cell-number='3750']");
+pen.style.backgroundColor = "black";
 window.addEventListener("keydown", () => movePen(window.event.key));
 
 function createGrid() {
     const grid = document.querySelector("#grid");
 
-    for(let i = 0; i < 7500; i++) {
+    for (let i = 0; i < 7500; i++) {
         const cell = document.createElement("div");
         cell.className = "cell";
         cell.setAttribute("data-cell-number", `${i}`);
-        cell.style.padding = `${grid.style.width/100}`;
+        cell.style.padding = `${grid.style.width / 100}`;
 
         grid.appendChild(cell);
     }
 }
 
 function movePen(key) {
+    let newPenPosition;
     switch (key) {
         case "h":
         case "ArrowLeft":
-            console.log("left");
+            newPenPosition = pen.dataset.cellNumber - 1;
+            colorInCell(newPenPosition);
             break;
         case "j":
         case "ArrowDown":
-            console.log("down");
+            newPenPosition = Number(pen.dataset.cellNumber) + 100;
+            colorInCell(newPenPosition);
             break;
         case "k":
         case "ArrowUp":
-            console.log("up");
+            newPenPosition = pen.dataset.cellNumber - 100;
+            colorInCell(newPenPosition);
             break;
         case "l":
         case "ArrowRight":
-            console.log("right");
+            newPenPosition = Number(pen.dataset.cellNumber) + 1;
+            colorInCell(newPenPosition);
             break;
     };
-    
+
+}
+
+function colorInCell(cell) {
+    pen = document.querySelector(`[data-cell-number='${cell}'`);
+    pen.style.backgroundColor = "black";
 }
