2490adcff41e96ae5f7600221da46f11676a7b43
Woo Han <hanwj100@gmail.com>
2022-08-11 10:53:30 -0700

    Create tentative movePen function
    The current goal is to be able to create an "iterator" or cursor which
    will select a single cell div and move to the corresponding div that is
    to the left, below, above, or right by using "hjkl" or arrow keys to
    navigate/move the cursor.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 createGrid();
 const grid = document.querySelector("#grid");
-grid.addEventListener("")
+window.addEventListener("keydown", () => movePen(window.event.key));
 
 function createGrid() {
     const grid = document.querySelector("#grid");
@@ -16,6 +16,23 @@ function createGrid() {
 }
 
 function movePen(key) {
-
+    switch (key) {
+        case "h":
+        case "ArrowLeft":
+            console.log("left");
+            break;
+        case "j":
+        case "ArrowDown":
+            console.log("down");
+            break;
+        case "k":
+        case "ArrowUp":
+            console.log("up");
+            break;
+        case "l":
+        case "ArrowRight":
+            console.log("right");
+            break;
+    };
     
 }
